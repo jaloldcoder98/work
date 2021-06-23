0b430139fec4bbd785be4a1789fc689fcf4b6bc6
Jaloliddin <jaloldcoder@gmail.com>
2021-06-23 19:06:46 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,193 @@
+"use strict";
+///////////////////////////////////
+
+//  lesson 10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////
+//  lesson 9//////////////////////////
+
+// console.log('arr' + " - object");
+
+// console.log(4 + +"5");
+
+
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+
+
+// console.log(5%2);
+
+// console.log(2*4 === 8);
+
+
+// const isChecked = false,
+//       isClose = false;
+// console.log(isChecked || !isClose);      
+
+
+// console.log(2 + 2 * 2 === 8);
+
+// console.log(2 + 2 * 2 != '6');
+
+// console.log(2 + 2 * 2 !== '6');
+
+
+
+
+// ///////////////////////////////
+// lesson 8
+
+// const category = 'toys';
+
+// console.log(`https://sumeurl.com/${category}/5`);
+
+// const user = 'Jalol';
+// alert(`Salom, ${user}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// lesson 7
+
+// alert(`hello`);
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+
+// const answer = +prompt("Siz 18 misiz?", "18")
+// console.log(answer + 5);
+
+
+// const answers = [];
+
+// // answers[0] = prompt('Kak vashe imya?', '');
+// // answers[1] = prompt('Kak vashe familya?', '');
+// // answers[2] = prompt('skolka vam let?', '');
+
+// console.log(typeof (answers));
+// console.log(typeof (null));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// lesson 6
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = "Jaloliddin ";
+
+// const bool = false;
+// console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj [`name`]);
+
+
+// let arr = ['plum.png', 'orange.png', 'apple.png', {}, []];
+
+// console.log(arr[0 ]);
+
+
+
+
+
+
+
+
+
+
+// 5 lesson
+// let number = 5;
+// const leftBorderWidth = 1;
+
+
+// number = 10;
+// console.log(number);
+
+
+// const obj = {
+//     a:50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
