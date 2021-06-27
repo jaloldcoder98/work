e850face5f6d27b28745929e9ab6c11fa56689c0
Jaloliddin <jaloldcoder@gmail.com>
2021-06-27 22:08:35 +0500

    callback comment

--- js/script.js
+++ js/script.js
@@ -1,9 +1,178 @@
-"use strict";
+" use strict "; 
+
+
+// Lesson 17
+
+const str = "test";
+
+
+// console.log(str[2] = 'd');
+
+// console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("q"));
+
+
+const logg = "Hello world Jaloliddin";
+
+// console.log(logg.slice(6, 11));
+
+
+// console.log(logg.substring(6, 11));
+
+
+console.log(logg.substr(6, 16));
+
+
+
+const num = 12.2;
+
+console.log(Math.round(num));
+
+const test = "12.2px";
+// console.log(parseInt(test));
+console.log(parseFloat(test));
+
+
+
+
+
+
+
+// Lesson 16
+
+// let  num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 220;
+//     console.log(num);
+// }
+
+// showFirstMessage("Hello World!");
+
+// console.log(num);
+
+// function calc(a , b) {
+//     return (a + b);
+    
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(44, 13));
+// console.log(calc(47, 30));
+
+// function ret() {
+//     let num = 50;
+//     // 
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+// const logger = function() {
+//     console.log("Hello");
+// };
+// logger();
+
+
+// const calc = (a, b)  => {
+//     console.log('1');
+//     return a +b;
+// };
+
+
+
+
+
+
+// lesson 14
+// let num = 50;
+ 
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 8; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ///////////////////////////////////
 
-//  lesson 10
+//  lesson 13
+
+// if(1) {
+//     console.log('Ok');
+// } else {
+//     console.log('Erorr');
+// }
 
 
+// if (num < 49) {
+//     console.log('Erorr');
+// }else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok');
+// }
+
+// (num === 50) ? console.log('Ok') : console.log('Erorr');
+
+// const num = 50;
+
+// switch (num) {
+//     case 49:
+//         console.log('Erorr');
+//         break;
+//     case 100:
+//         console.log('Erorr');
+//         break;
+//     case 51:
+//         console.log('V tochu!');
+//         break;
+//     default:
+//         console.log('Ne v etot raz');
+//         break;              
+// }
 
 
 
